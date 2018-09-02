3082aa39b6e98ec5eccb744b5f9114aef1901b30
Changhoon Yoon <changhoon.yoon@hstattler1-mp92.local>
2018-09-02 14:18:08 -0400

    Revert "Small changes"
    This reverts commit 4237d1e7fbd99190603325c40a57f049aeb0cc53.

--- js/script.js
+++ js/script.js
@@ -4,13 +4,24 @@
 	if ( $(window).width() > 739) {
 		//var $navBar = $('.navBar');
 		var $win = $(window);
+		/*
+		//Fixed Navigation Bar
+		$win.on('scroll', function() {
+			var top = $win.scrollTop();
+			$navBar.css('top', '' + top + 'px');
+			console.log(top);
+		});
+
 
 		//Parallax circles
-		var $circles = $('.circles');
 		var $circle1 = $('.circle1');
 		var $circle2 = $('.circle2');
+		var $circle3 = $('.circle3');
+		var $circle4 = $('.circle4');
+		var $circle5 = $('.circle5');
 
 		$win.on('scroll', function() {
+<<<<<<< HEAD
 			var top1 = $win.scrollTop()/800;
 			var top2 = ($win.scrollTop() - 150);
 			var top3 = $win.scrollTop() + 50;
@@ -20,6 +31,19 @@
 			$circle1.css('margin-left', (top3.toFixed(1) * -1) + 'px');
 			$circle2.css('margin-left', (top4.toFixed(1)) + 'px');
 		});
+=======
+			var top1 = $win.scrollTop()/3;
+			var top2 = $win.scrollTop()/2.5;
+			var top3 = $win.scrollTop()/1.5;
+			var top4 = $win.scrollTop()/2;
+			var top5 = $win.scrollTop()/6;
+			$circle1.css('transform', 'translateY(' + top1.toFixed(1) + 'px)');
+			$circle2.css('transform', 'translateY(' + top2.toFixed(1) + 'px)');
+			$circle3.css('transform', 'translateY(' + top3.toFixed(1) + 'px)');
+			$circle4.css('transform', 'translateY(' + top4.toFixed(1) + 'px)');
+			$circle5.css('transform', 'translateY(' + top5.toFixed(1) + 'px)');
+		});*/
+>>>>>>> parent of 4237d1e... Small changes
 
 	}*/
 
@@ -51,7 +75,7 @@
   //******Hiding Header******
   var didScroll;
   var lastScrollTop = 0;
-  var delta = 0;
+  var delta = 5;
   var navbarHeight = 100;
   // on scroll, let the interval function know the user has scrolled
   $(window).scroll(function(event){
@@ -63,7 +87,7 @@
       hasScrolled();
       didScroll = false;
     }
-  }, 0);
+  }, 250);
   function hasScrolled() {
     var st = $(this).scrollTop();
 
